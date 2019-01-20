c85743220d2be13425597d6a359d6e35c934768e
Enig <eestevez8091@gmail.com>
2019-01-20 15:01:22 -0500

    lol

---
+++ chapter 2/chap2_2.32/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Enigmatic
+ *
+ * Created on January 20, 2019, 3:00 PM
+ */
+//Exercise 2.32: Is the following code legal or not? If not, how might you
+//make it legal?
+
+//int null = 0, *p = null; ILLEGAL
+//LEGAL : int null = 0, *p= &null;
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
