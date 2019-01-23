297163387bc1ce60d44f5fc3d633275274df4143
Enig <eestevez8091@gmail.com>
2019-01-23 12:09:47 -0500

    .

---
+++ chapter 2/chap2_2.40/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Enigmatic
+ *
+ * Created on January 23, 2019, 12:07 PM
+ */
+//Exercise 2.40: Write your own version of the Sales_data class.
+
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+struct Sales_data{
+    string bookNo;
+    unsigned units_sold;
+    double revenue;
+};
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
