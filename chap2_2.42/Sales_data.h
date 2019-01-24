ded19f1376d1ef8fed12cdb5857939dc7a937a9d
unknown <eestevez8091@gmail.com>
2019-01-24 13:19:34 -0500

    .

---
+++ chap2_2.42/Sales_data.h
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Sales_data.h
+ * Author: enrique
+ *
+ * Created on January 24, 2019, 1:13 PM
+ */
+
+struct Sales_data{
+    std::string bookNo;
+    unsigned units_sold;
+    double revenue;
+};
+
