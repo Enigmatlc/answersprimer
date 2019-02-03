b85d91bcaad27fa1e29dd5168df4ee3d60509650
Enigmatlc <eestevez8091@gmail.com>
2019-02-03 14:59:27 -0500

    .

---
+++ chapter 3/chap3_3.12/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Enigmatic
+ *
+ * Created on February 3, 2019, 2:52 PM
+ */
+
+//Exercise 3.12: Which, if any, of the following vector definitions are in
+//error? For those that are legal, explain what the definition does. For those
+//that are not legal, explain why they are illegal.
+//(a) vector<vector<int>> ivec; declares an empty vector of vector<int>
+//(b) vector<string> svec = ivec; ILLEGAL: Tries to initialize a a vector<string> from a vector of vector<int>
+//(c) vector<string> svec(10, "null"); string vector with ten copies of the string "null"
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
