dff52afa4aa2b54412bca377bf34896975a91372
Enigmatic <eestevez8091@gmail.com>
2019-02-06 22:51:32 -0500

    .

--- chapter 3/chap3_3.2/main.cpp
+++ chapter 3/chap3_3.2/main.cpp
@@ -13,12 +13,6 @@
 //Exercise 3.2: Write a program to read the standard input a line at a time.
 //Modify your program to read a word at a time.
 
-<<<<<<< HEAD
-=======
-//Exercise 3.2: Write a program to read the standard input a line at a time.
-//Modify your program to read a word at a time.
-
->>>>>>> 956a3ee19184e162c78232282975a3d38f574b7e
 #include <iostream>
 #include <string>
 using std::string;      using std::cout;     using std::cin;
@@ -26,6 +20,7 @@ using std::cin;         using std::endl;     using std::getline;
 /*
  * 
  */
+
 int main() {
     string line;
     //read line
