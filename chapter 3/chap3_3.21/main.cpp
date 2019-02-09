5f9e57652a78c59e8935e90f88b968efd8429338
Enigmatic <eestevez8091@gmail.com>
2019-02-08 19:37:08 -0500

    .

--- chapter 3/chap3_3.21/main.cpp
+++ chapter 3/chap3_3.21/main.cpp
@@ -8,44 +8,73 @@
  * File:   main.cpp
  * Author: Enigmatic
  *
- * Created on February 7, 2019, 2:12 PM
+ * Created on February 8, 2019, 7:28 PM
  */
-
-//Exercise 3.24: Redo the last exercise from § 3.3.3 (p. 105) using iterators
-
+//Exercise 3.21: Redo the first exercise from § 3.3.3 (p. 105) using iterators.
 //3.3.3
-//Exercise 3.20: Read a set of integers into a vector. Print the sum of each
-//pair of adjacent elements. Change your program so that it prints the sum of
-//the first and last elements, followed by the sum of the second and second-tolast,
-//and so on.
-
-#include <iostream>
+//Exercise 3.16: Write a program to print the size and contents of the
+//vectors from exercise 3.13. Check whether your answers to that exercise
+//were correct. If not, restudy § 3.3.1 (p. 97) until you understand why you
+//were wrong.
 #include <vector>
+#include <iostream>
+#include <string>
 
-using std::vector;  using std::cin;
-using std::cout;    using std::endl;
-
-
+using std::vector;  using std::cout;
+using std::endl;    using std::string;
 /*
  * 
  */
 int main(int argc, char** argv) {
-    vector<int> vec;
-    for(int read=0;cin>>read;vec.push_back(read));
-    //part a
-    for(auto iter = vec.cbegin();iter != vec.cend()-1;++iter){
-        cout<<*iter+*(iter+1)<<" ";
-        
+    //v1
+    vector<int> v1;
+    cout<<"V1"<<" size: "<<v1.size()<<endl;
+    for(auto iter=v1.cbegin();iter!=v1.cend();iter++){
+        cout<<*iter<<" ";
+    }
+     cout<<endl;
+    //v2
+    vector<int> v2(10);
+    cout<<"V2"<<" size: "<<v2.size()<<endl;
+    for(auto iter=v2.cbegin();iter!=v2.cend();iter++){
+        cout<<*iter<<" ";
     }
     cout<<endl;
-    //part b
-    auto end =vec.cend()-1;
-    for(auto iter = vec.cbegin();iter != vec.cend();iter++){
-        if(iter!=end && !(iter>end))
-            cout<<*iter+*end<<" ";
-        --end;
-        
+    //v3
+    vector<int> v3(10, 42);
+    cout<<"V3"<<" size: "<<v3.size()<<endl;
+    for(auto iter=v3.cbegin();iter!=v3.cend();iter++){
+        cout<<*iter<<" ";
     }
+    cout<<endl;
+    //v4
+    vector<int> v4{10};
+    cout<<"V4"<<" size: "<<v4.size()<<endl;
+    for(auto iter=v4.cbegin();iter!=v4.cend();iter++){
+        cout<<*iter<<" ";
+    }
+    cout<<endl;
+    //v5
+    vector<int> v5{10, 42};
+    cout<<"V5"<<" size: "<<v5.size()<<endl;
+   for(auto iter=v5.cbegin();iter!=v5.cend();iter++){
+        cout<<*iter<<" ";
+    }
+    cout<<endl;
+    //v6
+    vector<string> v6{10};
+    cout<<"V6"<<" size: "<<v6.size()<<endl;
+   for(auto iter=v6.cbegin();iter!=v6.cend();iter++){
+        cout<<*iter<<" ";
+    }
+    cout<<endl;
+    //v7
+    vector<string> v7{10, "hi"};
+    cout<<"V7"<<" size: "<<v7.size()<<endl;
+   for(auto iter=v7.cbegin();iter!=v7.cend();iter++){
+        cout<<*iter<<" ";
+    }
+    cout<<endl;
+    
     return 0;
 }
-
---
+++ chapter 3/chap3_3.24/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Enigmatic
+ *
+ * Created on February 7, 2019, 2:12 PM
+ */
+
+//Exercise 3.24: Redo the last exercise from § 3.3.3 (p. 105) using iterators
+
+//3.3.3
+//Exercise 3.20: Read a set of integers into a vector. Print the sum of each
+//pair of adjacent elements. Change your program so that it prints the sum of
+//the first and last elements, followed by the sum of the second and second-tolast,
+//and so on.
+
+#include <iostream>
+#include <vector>
+
+using std::vector;  using std::cin;
+using std::cout;    using std::endl;
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    vector<int> vec;
+    for(int read=0;cin>>read;vec.push_back(read));
+    //part a
+    for(auto iter = vec.cbegin();iter != vec.cend()-1;++iter){
+        cout<<*iter+*(iter+1)<<" ";
+        
+    }
+    cout<<endl;
+    //part b
+    auto end =vec.cend()-1;
+    for(auto iter = vec.cbegin();iter != vec.cend();iter++){
+        if(iter!=end && !(iter>end))
+            cout<<*iter+*end<<" ";
+        --end;
+        
+    }
+    return 0;
+}
