987767235104aaab98ebe0e7dec343f61eb9259e
Enigmatic <eestevez8091@gmail.com>
2019-02-12 19:42:52 -0500

    .

--- chapter 3/chap3_3.36/main.cpp
+++ chapter 3/chap3_3.36/main.cpp
@@ -12,7 +12,7 @@
  */
 //Exercise 3.36: Write a program to compare two arrays for equality. Write a
 //similar program to compare two vectors.
-#include <cstdlib>
+#include <iostream>
 #include <iterator>
 #include <vector>
 using std::cout;    using std::endl;
@@ -25,7 +25,29 @@ using std::vector;
 //not finished.
 int main(int argc, char** argv) {
     int arr[]={1,2,3,4,5};
+    int arr2[]={1,2,3,4,5};
+    vector<int> vec={1,2,3,4,5};
+    vector<int> vec2={1,2,3,4,5};
     auto arbegin=begin(arr),arend=end(arr);
+    auto arbegin2=begin(arr2),arend2=end(arr2);
+    //vector comparison
+    if(vec == vec2)
+        cout<<"vectors are equal"<<endl;
+    else
+        cout<<"vectors are not equal"<<endl;
+    //array comparison
+    while(arbegin != arend && arbegin2 != arend2 && arbegin != arend && arbegin2 != arend2){
+        if(*arbegin == *arbegin2){
+            cout<<"array is equal"<<endl;
+        }
+        else{
+            cout<<"array is not equal"<<endl;
+            return -1;
+        }
+        arbegin++;
+        arbegin2++;
+                    
+    }
     
     return 0;
 }
