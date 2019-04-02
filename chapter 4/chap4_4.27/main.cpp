29a50c1f5cf17b16d31b165fc61d63710da06fc2
unknown <eestevez8091@gmail.com>
2019-04-02 14:13:59 -0400

    .

---
+++ chapter 4/chap4_4.27/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Enigmatic
+ *
+ * Created on April 2, 2019, 2:09 PM
+ */
+
+//Exercise 4.27: What is the result of each of these expressions?
+//unsigned long ul1 = 3, ul2 = 7;
+
+//(a) ul1 & ul2 0000011 & 00000111 = 00000011(3)
+//(b) ul1 | ul2 0000011 | 00000111 = 00000111(7)
+//(c) ul1 && ul2 true(1)
+//(d) ul1 || ul2 true(1)
