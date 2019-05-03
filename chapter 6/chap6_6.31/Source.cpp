3db608118368bf42476fa48e1fa1341e5b08d8e7
Enigmatlc <30873095+Enigmatlc@users.noreply.github.com>
2019-05-03 15:31:11 -0400

    .

---
+++ chapter 6/chap6_6.31/Source.cpp
@@ -0,0 +1,4 @@
+//Exercise 6.31: When is it valid to return a reference ? A reference to
+//const ?
+
+//Its valid to return a reference when it doesn't refer to a local object.We can return a const reference when ever we don't want to assign to the return of the call.
---
+++ chapter 6/chap6_6.35/Source.cpp
@@ -0,0 +1,3 @@
+//Exercise 6.35: In the call to fact, why did we pass val - 1 rather than
+//val-- ?
+//?
