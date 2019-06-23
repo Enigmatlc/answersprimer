8b697ffeadb96611c02b1d8b8dd0836cd48fee4f
Enigmatlc <30873095+Enigmatlc@users.noreply.github.com>
2019-06-23 12:09:42 -0400

    .

---
+++ chapter 6/chap6_6.43/Source.cpp
@@ -0,0 +1,8 @@
+//Exercise 6.43: Which one of the following declarations and definitions would
+//you put in a header ? In a source file ? Explain why.
+
+//i would put both functions in the header file
+
+//(a) inline bool eq(const BigInt&, const BigInt&) { ... }
+//
+//(b) void putValues(int* arr, int size);
