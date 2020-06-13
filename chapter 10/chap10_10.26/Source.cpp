44d1407dc8f3b0e01ecbb728343ded5f41cb3329
Enigmatlc <30873095+Enigmatlc@users.noreply.github.com>
2020-06-12 20:11:30 -0400

    .

---
+++ chapter 10/chap10_10.26/Source.cpp
@@ -0,0 +1,6 @@
+//Exercise 10.26: Explain the differences among the three kinds of insert
+//iterators.
+
+//back_inserter creates na interator that uses the push_back member of the container to push items in the back of the iterator.
+//front_inserter creates an interator that puts items in the front of the container by using push front.
+//inserter inserts and intem at th desired location by using the insert member of the container
